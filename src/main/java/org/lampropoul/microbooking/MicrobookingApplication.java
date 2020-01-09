3ae7498c669ed4433a1f7245270d28e04fe145ae
Vassilis Lambropoulos <lampropoul1989@gmail.com>
2020-01-09 09:45:15 +0200

    Initial commit.

---
+++ src/main/java/org/lampropoul/microbooking/MicrobookingApplication.java
@@ -0,0 +1,13 @@
+package org.lampropoul.microbooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicrobookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicrobookingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/lampropoul/microbooking/MicrobookingApplicationTests.java
@@ -0,0 +1,13 @@
+package org.lampropoul.microbooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicrobookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
