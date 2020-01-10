338ee2667e646591f85cd2529929e4c0f627fdfe
Vassilis Lambropoulos <lampropoul1989@gmail.com>
2020-01-10 14:26:58 +0200

    Add seed data.

--- src/main/java/org/lampropoul/microbooking/web/HotelsController.java
+++ src/main/java/org/lampropoul/microbooking/web/HotelsController.java
@@ -1,12 +1,9 @@
 package org.lampropoul.microbooking.web;
 
 import lombok.RequiredArgsConstructor;
-import org.lampropoul.microbooking.model.Booking;
 import org.lampropoul.microbooking.model.Hotel;
 import org.lampropoul.microbooking.repositories.BookingRepository;
 import org.lampropoul.microbooking.repositories.HotelRepository;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,26 +20,5 @@ public class HotelsController {
     @GetMapping("/{surname}")
     public Iterable<Hotel> all(@PathVariable String surname) {
         return hotelRepository.findAllBySurname(surname);
-    }
-
-    @GetMapping("/seed")
-    public ResponseEntity<Booking> seed() {
-        Hotel hotel = new Hotel();
-        hotel.setName("Hyatt Place");
-        hotel.setAddress("56 W 36th St, New York, NY");
-        Hotel hyatt = hotelRepository.save(hotel);
-
-        Booking booking = new Booking();
-        booking.setCustomerName("Vassilis");
-        booking.setCustomerSurname("Lambropoulos");
-        booking.setHotel(hyatt);
-        bookingRepository.save(booking);
-
-        Booking booking2 = new Booking();
-        booking2.setCustomerName("Konstantina");
-        booking2.setCustomerSurname("Manika");
-        booking2.setHotel(hyatt);
-
-        return new ResponseEntity<>(bookingRepository.save(booking2), HttpStatus.OK);
     }
 }
---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO hotel (id, name, address, rating)
+VALUES (1, 'Hyatt Place', '56 W 36th St, New York, NY', 4);
+
+INSERT INTO booking (id, customer_name, customer_surname, pax, hotel_id)
+VALUES (2, 'Vassilis', 'Lambropoulos', 1, 1),
+       (3, 'Konstantina', 'Lambropoulos', 1, 1);
