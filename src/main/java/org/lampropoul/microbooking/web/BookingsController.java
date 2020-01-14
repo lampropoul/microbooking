8cd63188dfa40a6aa0f652f2be971c9a7e707568
Vassilis Lambropoulos <lampropoul1989@gmail.com>
2020-01-14 18:34:42 +0200

    Add JavaDoc.

--- src/main/java/org/lampropoul/microbooking/web/BookingsController.java
+++ src/main/java/org/lampropoul/microbooking/web/BookingsController.java
@@ -12,6 +12,9 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Optional;
 
+/**
+ * Web controller that accepts requests on /bookings
+ */
 @RestController
 @RequestMapping("/bookings")
 @RequiredArgsConstructor
@@ -20,11 +23,18 @@ public class BookingsController {
     private final BookingRepository bookingRepository;
     private final HotelRepository hotelRepository;
 
+    /**
+     * @return all bookings on DB and HTTP status of 200 OK
+     */
     @GetMapping("/all")
     public ResponseEntity<Iterable<Booking>> all() {
         return new ResponseEntity<>(bookingRepository.findAll(), HttpStatus.OK);
     }
 
+    /**
+     * @param id primary key
+     * @return a booking and HTTP status of 200 OK or 204 NO_CONTENT
+     */
     @GetMapping("/{id}")
     public ResponseEntity<Booking> byId(@PathVariable Long id) {
         Optional<Booking> booking = bookingRepository.findById(id);
@@ -33,6 +43,10 @@ public class BookingsController {
                 .orElseGet(() -> new ResponseEntity<>(HttpStatus.NO_CONTENT));
     }
 
+    /**
+     * @param booking request object in HTTP body
+     * @return a booking and HTTP status of 201 CREATED or 400 BAD_REQUEST
+     */
     @PostMapping
     public ResponseEntity<Booking> create(@RequestBody Booking booking) {
         if (bookingRepository.findById(booking.getId()).isPresent()) {
@@ -43,6 +57,10 @@ public class BookingsController {
         }
     }
 
+    /**
+     * @param booking request object in HTTP body
+     * @return a booking and HTTP status of 200 OK or 400 BAD_REQUEST
+     */
     @PutMapping
     public ResponseEntity<Booking> update(@RequestBody Booking booking) {
         if (bookingRepository.findById(booking.getId()).isPresent()) {
@@ -53,6 +71,10 @@ public class BookingsController {
         }
     }
 
+    /**
+     * @param id the primary key
+     * @return a booking and HTTP status of 204 NO_CONTENT or 400 BAD_REQUEST
+     */
     @DeleteMapping("/{id}")
     public ResponseEntity<Booking> delete(@PathVariable Long id) {
         if (bookingRepository.findById(id).isPresent()) {
@@ -63,11 +85,20 @@ public class BookingsController {
         }
     }
 
+    /**
+     * @param hotelName name of the hotel
+     * @return a list with all the booking that this hotel has and HTTP status of 200 OK
+     */
     @GetMapping("/byHotelName/{hotelName}")
     public ResponseEntity<List<Booking>> getAllBookingsForAHotel(@PathVariable String hotelName) {
         return new ResponseEntity<>(bookingRepository.getAllByHotel(hotelName), HttpStatus.OK);
     }
 
+    /**
+     * @param booking a booking object
+     * @return true if hotel is null, false if the hotel is either new or exists
+     * in the case that the hotel does not exist, it gets persisted
+     */
     private boolean hotelIsNull(Booking booking) {
         Hotel hotel = booking.getHotel();
         if (hotel == null) {
--- src/main/java/org/lampropoul/microbooking/web/HotelsController.java
+++ src/main/java/org/lampropoul/microbooking/web/HotelsController.java
@@ -10,6 +10,9 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Optional;
 
+/**
+ * Web controller that accepts requests on /hotels
+ */
 @RestController
 @RequestMapping("/hotels")
 @RequiredArgsConstructor
@@ -17,11 +20,18 @@ public class HotelsController {
 
     private final HotelRepository hotelRepository;
 
+    /**
+     * @return all hotels on DB and HTTP status of 200 OK
+     */
     @GetMapping("/all")
     public ResponseEntity<Iterable<Hotel>> all() {
         return new ResponseEntity<>(hotelRepository.findAll(), HttpStatus.OK);
     }
 
+    /**
+     * @param id primary key
+     * @return a hotel and HTTP status of 200 OK or 204 NO_CONTENT
+     */
     @GetMapping("/{id}")
     public ResponseEntity<Hotel> byId(@PathVariable Long id) {
         Optional<Hotel> hotel = hotelRepository.findById(id);
@@ -30,6 +40,10 @@ public class HotelsController {
                 .orElseGet(() -> new ResponseEntity<>(HttpStatus.NO_CONTENT));
     }
 
+    /**
+     * @param hotel request object in HTTP body
+     * @return a hotel and HTTP status of 201 CREATED or 400 BAD_REQUEST
+     */
     @PostMapping
     public ResponseEntity<Hotel> create(@RequestBody Hotel hotel) {
         if (hotelRepository.findById(hotel.getId()).isPresent()) {
@@ -39,6 +53,10 @@ public class HotelsController {
         }
     }
 
+    /**
+     * @param hotel request object in HTTP body
+     * @return a hotel and HTTP status of 200 OK or 400 BAD_REQUEST
+     */
     @PutMapping
     public ResponseEntity<Hotel> update(@RequestBody Hotel hotel) {
         if (hotelRepository.findById(hotel.getId()).isPresent()) {
@@ -48,6 +66,10 @@ public class HotelsController {
         }
     }
 
+    /**
+     * @param id the primary key
+     * @return a hotel and HTTP status of 204 NO_CONTENT or 400 BAD_REQUEST
+     */
     @DeleteMapping("/{id}")
     public ResponseEntity<Hotel> delete(@PathVariable Long id) {
         if (hotelRepository.findById(id).isPresent()) {
@@ -58,12 +80,20 @@ public class HotelsController {
         }
     }
 
+    /**
+     * @param surname a client's surname
+     * @return get all hotels this client has booked for
+     */
     @GetMapping("/withBookingsFor/{surname}")
     public ResponseEntity<List<Hotel>> hotelsThatHaveBookingsForSurname(@PathVariable String surname) {
         List<Hotel> hotels = hotelRepository.findAllBySurname(surname);
         return new ResponseEntity<>(hotels, HttpStatus.OK);
     }
 
+    /**
+     * @param id hotel id
+     * @return the sum of the amounts of all bookings this hotel has
+     */
     @GetMapping("/{id}/priceAmounts")
     public ResponseEntity<Float> priceAmounts(@PathVariable Long id) {
         return new ResponseEntity<>(hotelRepository.calculatePriceAmounts(id), HttpStatus.OK);
